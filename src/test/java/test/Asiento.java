3ce76a0b7808387676d83b780029cb01aa9a1fc6
RBucheli8 <141966641+RBucheli8@users.noreply.github.com>
2023-08-20 17:05:33 -0500

    Asiento
    Corrección clase asiento

--- src/test/java/test/Asiento.java
+++ src/test/java/test/Asiento.java
@@ -1,18 +1,13 @@
 package test;
 
-import java.util.Arrays;
-
 public class Asiento {
 	String color;
 	int precio;
 	int registro;
 	
 	void cambiarColor(String color) {
-		String[] colores = {"rojo", "verde", "amarillo", "negro", "blanco"};
-		
-		if (Arrays.asList(colores).contains(color)) {
+		if (color == "rojo" || color == "verde" || color ==  "amarillo" || color ==  "negro" || color ==  "blanco") {
 			this.color = color;
 		}
 	}
-
 }
